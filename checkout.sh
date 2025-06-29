367e1add2736a5b0941f7cba74c76328a85a3f58
pkgdemon <pkgdemonteam@gmail.com>
2025-06-29 13:43:12 -0500

    Fix checkout

--- checkout.sh
+++ checkout.sh
@@ -12,7 +12,7 @@ https://github.com/gershwin-desktop/gershwin-workspace.git
 https://github.com/gershwin-desktop/gershwin-systempreferences.git
 https://github.com/gershwin-desktop/gershwin-globaldefaults.git
 https://github.com/gershwin-desktop/ghostbsd-gershwin-settings.git
--b mactinosh https://github.com/gershwin-desktop/gershwin-rik-theme.git
+https://github.com/gershwin-desktop/gershwin-rik-theme.git
 "
 
 mkdir -p repos

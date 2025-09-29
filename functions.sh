0f96cbd46c21447f6f66ae01672f3599abecda87
probonopd <probonopd@users.noreply.github.com>
2025-09-29 14:06:05 +0200

    Detect OS using /etc/os-release
    Similar to bootstrap.sh

--- functions.sh
+++ functions.sh
@@ -1,39 +1,85 @@
 #!/bin/sh
 
-# Detect platform and define tools accordingly
-detect_platform() {
-    OS=$(uname -s)
-    case "$OS" in
+set -eux
+
+# Detect OS_ID and OS_LIKE
+OS_ID=""
+OS_LIKE=""
+
+if [ -f /etc/os-release ]; then
+    # shellcheck disable=SC1091
+    . /etc/os-release
+    OS_ID=${ID:-}
+    OS_LIKE=${ID_LIKE:-}
+else
+    case "$(uname -s)" in
         FreeBSD)
-            PLATFORM="freebsd"
-            MAKE_CMD="gmake"
-            NPROC_CMD="sysctl -n hw.ncpu"
-            ;;
-        GhostBSD)
-            PLATFORM="ghostbsd"
-            MAKE_CMD="gmake"
-            NPROC_CMD="sysctl -n hw.ncpu"
-            ;;
-        Linux)
-            if [ -f /etc/arch-release ]; then
-                PLATFORM="arch"
-                MAKE_CMD="make"
-                NPROC_CMD="nproc"
-            elif  [ -f /etc/debian-release ]; then
-                PLATFORM="debian"
-                MAKE_CMD="make"
-                NPROC_CMD="nproc"
-            else
-                echo "Unsupported Linux distribution"
-                exit 1
-            fi
+            OS_ID=freebsd
+            OS_LIKE=
             ;;
         *)
-            echo "Unsupported OS: $OS"
+            printf '%s\n' "Unsupported or unknown OS." >&2
             exit 1
             ;;
     esac
-}
+fi
+
+printf 'Detected OS: %s\n' "$OS_ID"
+
+# Map OS_ID / OS_LIKE to PLATFORM, MAKE_CMD, NPROC_CMD
+PLATFORM=""
+MAKE_CMD=""
+NPROC_CMD=""
+
+case "$OS_ID" in
+    freebsd|ghostbsd)
+        PLATFORM=$OS_ID
+        MAKE_CMD=gmake
+        NPROC_CMD='sysctl -n hw.ncpu'
+        ;;
+    arch)
+        PLATFORM=arch
+        MAKE_CMD=make
+        NPROC_CMD=nproc
+        ;;
+    debian|ubuntu|linuxmint)
+        PLATFORM=debian
+        MAKE_CMD=make
+        NPROC_CMD=nproc
+        ;;
+    *)
+        if [ -n "$OS_LIKE" ]; then
+            case "$OS_LIKE" in
+                debian*)
+                    PLATFORM=debian
+                    MAKE_CMD=make
+                    NPROC_CMD=nproc
+                    ;;
+                arch*)
+                    PLATFORM=arch
+                    MAKE_CMD=make
+                    NPROC_CMD=nproc
+                    ;;
+                freebsd*)
+                    PLATFORM=freebsd
+                    MAKE_CMD=gmake
+                    NPROC_CMD='sysctl -n hw.ncpu'
+                    ;;
+                *)
+                    printf '%s\n' "Unsupported or unknown Linux distribution: $OS_ID (ID_LIKE=$OS_LIKE)" >&2
+                    exit 1
+                    ;;
+            esac
+        else
+            printf '%s\n' "Unsupported or unknown OS: $OS_ID" >&2
+            exit 1
+        fi
+        ;;
+esac
+
+printf 'Platform: %s\n' "$PLATFORM"
+printf 'Make command: %s\n' "$MAKE_CMD"
+printf 'Nproc command: %s\n' "$NPROC_CMD"
 
 # Determine CPU count for parallel builds
 get_cpu_count() {

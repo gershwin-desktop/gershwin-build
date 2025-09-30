c1a6a3418931a42295a85c43b856bf188eceaa87
probonopd <probonopd@users.noreply.github.com>
2025-09-30 12:24:15 +0200

    Add comment [skip ci]

--- install-system-domain.sh
+++ install-system-domain.sh
@@ -89,6 +89,7 @@ $MAKE_CMD clean
 
 cd "$REPOS_DIR/gershwin-universe-apps/Terminal"
 # On glibc based Linux systems, -liconv should not be used as iconv is part of glibc
+# TODO: Port this fix to GNUmakefile.preamble properly
 if [ $(uname) == "Linux" ] ; then
   sed -i -e 's|-liconv ||g' GNUmakefile.preamble
 fi

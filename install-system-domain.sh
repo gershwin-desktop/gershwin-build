63de11a2b040700fb8c26bfaba4c333968d03223
probonopd <probonopd@users.noreply.github.com>
2025-12-29 06:16:09 +0100

    Make NimbusSans font removal command more generic

--- install-system-domain.sh
+++ install-system-domain.sh
@@ -146,7 +146,7 @@ cp fonts/*.otf "$DEST"/ || exit 1
 cp -r fontconfig /System/Library/Preferences/Fontconfig || exit 1
 # Use fixed Nimbus Sans from protamail/NimbusSans (replaces URW version)
 # See: https://github.com/ArtifexSoftware/urw-base35-fonts/issues/25
-rm -f "$DEST"/NimbusSans*.otf
+rm -f "$DEST"/NimbusSans*
 cd "$REPOS_DIR/NimbusSans"
 cp NimbusSans*.ttf "$DEST"/ || exit 1
 find "$DEST"

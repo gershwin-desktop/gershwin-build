efcade0225e0c96f257f7ea1e61fd9cbb6bed519
pkgdemon <pkgdemonteam@gmaill.com>
2026-01-19 00:18:13 -0600

    Use upstream repos for core libs

--- checkout.sh
+++ checkout.sh
@@ -3,11 +3,12 @@
 set -e
 
 REPOS="
+https://github.com/apple/swift-corelibs-libdispatch.git
 https://github.com/gnustep/libobjc2.git
-https://github.com/gershwin-desktop/tools-make.git
-https://github.com/gershwin-desktop/libs-base.git
-https://github.com/gershwin-desktop/libs-gui.git
-https://github.com/gershwin-desktop/libs-back.git
+https://github.com/gnustep/tools-make.git
+https://github.com/gnustep/libs-base.git
+https://github.com/gnustep/libs-gui.git
+https://github.com/gnustep/libs-back.git
 https://github.com/gershwin-desktop/gershwin-system.git
 https://github.com/gershwin-desktop/gershwin-workspace.git
 https://github.com/gershwin-desktop/gershwin-systempreferences.git

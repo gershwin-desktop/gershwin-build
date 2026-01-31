0ec99c034796768c046bad97900c5c813db521da
probonopd <probonopd@users.noreply.github.com>
2026-01-31 17:29:36 +0100

    Update -system to c38d6a0

--- checkout.sh
+++ checkout.sh
@@ -64,7 +64,7 @@ if [ "$PINNED" -eq 1 ]; then
     checkout_commit gershwin-assets              4deb482
     checkout_commit gershwin-components          3395d99
     checkout_commit gershwin-eau-theme           4babcb0
-    checkout_commit gershwin-system              655a065
+    checkout_commit gershwin-system              c38d6a0
     checkout_commit gershwin-systempreferences   8d49f50
     checkout_commit gershwin-terminal            71124e3
     checkout_commit gershwin-textedit            3df6db8

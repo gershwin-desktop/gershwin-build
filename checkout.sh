36bc6dff53fe93347c2f600a8630a2d782809996
probonopd <probonopd@users.noreply.github.com>
2026-02-01 10:21:12 +0100

    Pin fewer repos
    Previous commit resulted in a working system

--- checkout.sh
+++ checkout.sh
@@ -67,12 +67,13 @@ if [ "$PINNED" -eq 1 ]; then
     checkout_commit libs-gui                     8be638c
     checkout_commit swift-corelibs-libdispatch   ac3302c
     checkout_commit tools-make                   8964f83
-    checkout_commit gershwin-components          3395d99 // Bug is there even with this pinned
-    checkout_commit gershwin-eau-theme           4babcb0 // Bug is there even with this pinned
-    checkout_commit gershwin-windowmanager       1f3cc1c //  Is the culprit here?
+    // The culprit is in this repository:
+    checkout_commit gershwin-windowmanager       1f3cc1c
 fi
 
 # The following do not seeem to be causing the issue and are hence no longer pinned for the moment
+# checkout_commit gershwin-components          3395d99 // Bug is there even with this pinned
+# checkout_commit gershwin-eau-theme           4babcb0 // Bug is there even with this pinned
 # checkout_commit gershwin-assets              4deb482
 # checkout_commit gershwin-workspace           1bc3b98
 # checkout_commit gershwin-system              cdeafb6

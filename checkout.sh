2596b246b026e4fc6d3b19314bbb33bc6fc9daea
probonopd <probonopd@users.noreply.github.com>
2026-02-01 10:53:22 +0100

    # not //

--- checkout.sh
+++ checkout.sh
@@ -62,18 +62,18 @@ if [ "$PINNED" -eq 1 ]; then
     }
     # These are upstream libraries, we pin them in order to not develop for a moving target
     checkout_commit libobjc2                     4148a3d
-    checkout_commit libs-back                    bf3b3ce // Patch by okt
+    checkout_commit libs-back                    bf3b3ce # Patch by okt
     checkout_commit libs-base                    050c6cc
     checkout_commit libs-gui                     8be638c
     checkout_commit swift-corelibs-libdispatch   ac3302c
     checkout_commit tools-make                   8964f83
-    // The culprit is in this repository:
+    # The culprit is in this repository:
     checkout_commit gershwin-windowmanager       1f3cc1c
 fi
 
 # The following do not seeem to be causing the issue and are hence no longer pinned for the moment
-# checkout_commit gershwin-components          3395d99 // Bug is there even with this pinned
-# checkout_commit gershwin-eau-theme           4babcb0 // Bug is there even with this pinned
+# checkout_commit gershwin-components          3395d99 # Bug is there even with this pinned
+# checkout_commit gershwin-eau-theme           4babcb0 # Bug is there even with this pinned
 # checkout_commit gershwin-assets              4deb482
 # checkout_commit gershwin-workspace           1bc3b98
 # checkout_commit gershwin-system              cdeafb6

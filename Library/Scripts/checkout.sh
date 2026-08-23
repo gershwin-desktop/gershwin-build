37844d0ec29f908c89e7b60de30497ac54bc8abc
probonopd <probonopd@users.noreply.github.com>
2026-08-23 23:05:08 +0200

    Also clone docs
    For consumption by Help.app
    [ci skip]

--- Library/Scripts/checkout.sh
+++ Library/Scripts/checkout.sh
@@ -50,6 +50,7 @@ https://github.com/gershwin-desktop/gershwin-textedit.git
 https://github.com/gershwin-desktop/gershwin-windowmanager.git
 https://github.com/gershwin-desktop/gershwin-components.git
 https://github.com/gershwin-desktop/gershwin-assets.git
+https://github.com/gershwin-desktop/docs.git
 "
 
 # Pinned commits, as "<repo name> <commit>". These are upstream libraries; we pin
